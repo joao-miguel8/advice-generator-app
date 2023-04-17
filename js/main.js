316e812364b4aef0f039ed0c9c90591c4e34e1d2
[joao miguel] <[joaocun81@gmail.com]>
2023-04-17 11:33:45 -0400

    queried html elements to use in javascript file and created fetch method to retrieve data from API.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+const adviceCountNumber = document.querySelector(".advice-number");
+const quote = document.querySelector(".advice-generated");
 const generateQuoteButton = document.querySelector(".generate-new-advice");
 
-const quote = document.querySelector(".advice-generated");
+generateQuoteButton.addEventListener("click", generateAdvice);
+
+function generateAdvice() {
+    quote.textContent = quotePhrase;
+}
+
+let URL = "https://api.adviceslip.com/advice";
+
+// Fetch returns a promise - so you can use a .then method and a .catch method to handle the request.
+fetch(URL)
+    .then((response) => {
+        return response.json();
+        // handle response
+    })
+    .then((data) => {
+        const adviceCountNum = data.slip.id;
+        adviceCountNumber.textContent = adviceCountNum;
+        const quotePhrase = data.slip.advice;
+        // quote.textContent = quotePhrase;
+    });
+// .catch(error => console.log(error););
