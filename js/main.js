9ec402efeffc03808d1bfe1a222d22cab66603b8
[joao miguel] <[joaocun81@gmail.com]>
2023-04-17 14:39:19 -0400

    created an event listenerfor the button with a function to generate a new advice.

--- js/main.js
+++ js/main.js
@@ -4,22 +4,22 @@ const generateQuoteButton = document.querySelector(".generate-new-advice");
 
 generateQuoteButton.addEventListener("click", generateAdvice);
 
-function generateAdvice() {
-    quote.textContent = quotePhrase;
-}
-
-let URL = "https://api.adviceslip.com/advice";
-
 // Fetch returns a promise - so you can use a .then method and a .catch method to handle the request.
-fetch(URL)
-    .then((response) => {
-        return response.json();
-        // handle response
-    })
-    .then((data) => {
-        const adviceCountNum = data.slip.id;
-        adviceCountNumber.textContent = adviceCountNum;
-        const quotePhrase = data.slip.advice;
-        // quote.textContent = quotePhrase;
-    });
-// .catch(error => console.log(error););
+
+function generateAdvice() {
+    let URL = "https://api.adviceslip.com/advice";
+
+    // GETTING DATA FROM API
+    fetch(URL)
+        .then((response) => {
+            return response.json();
+            // handle response
+        })
+        .then((data) => {
+            const adviceCountNum = data.slip.id;
+            adviceCountNumber.textContent = adviceCountNum;
+            const generateQuote = data.slip.advice;
+            quote.textContent = generateQuote;
+        })
+        .catch((error) => console.log(error));
+}
